69009d493d5dae2fbe3dd34010103f10ff2b51af
Camila Morales <camila.moralesg21@gmail.com>
2022-05-25 21:34:30 -0500

    kata 4

--- src/main/java/katas/Kata4.java
+++ src/main/java/katas/Kata4.java
@@ -8,6 +8,7 @@ import util.DataUtil;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Retrieve id, title, and a 150x200 box art url for every video
@@ -18,6 +19,17 @@ public class Kata4 {
     public static List<Map> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
 
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys", "boxart", new BoxArt(150, 200, "url")));
+        List<Map> newListMovies =  movieLists.stream().flatMap((movieList) -> movieList.getVideos().stream())
+                .map(movie ->
+                        Map.of("id ", movie.getId(),
+                                "title ", movie.getTitle(),
+                                "boxart ", movie.getBoxarts().stream()
+                                        .filter(boxArt -> boxArt.getWidth() == 150 && boxArt.getHeight() == 200)
+                                        .map(boxArtUrl -> boxArtUrl.getUrl()).collect(Collectors.toList()))).collect(Collectors.toList());
+
+        System.out.println(newListMovies);
+        return newListMovies;
     }
-}
+
+    }
+
