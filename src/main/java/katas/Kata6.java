b0814822944f56c0c8bbce7876f819e730dc375d
Camila Morales <camila.moralesg21@gmail.com>
2022-05-25 23:47:52 -0500

    kata 6

--- src/main/java/katas/Kata6.java
+++ src/main/java/katas/Kata6.java
@@ -1,9 +1,13 @@
 package katas;
 
+import model.BoxArt;
 import model.Movie;
+import model.MovieList;
 import util.DataUtil;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
     Goal: Retrieve the url of the largest boxart using map() and reduce()
@@ -12,8 +16,16 @@ import java.util.List;
 */
 public class Kata6 {
     public static String execute() {
-        List<Movie> movies = DataUtil.getMovies();
+        List<MovieList> movies = DataUtil.getMovieLists();
 
-        return "someUrl";
+        String url = movies.stream().flatMap(movieList -> movieList.getVideos().stream())
+                .flatMap(movie1 -> movie1.getBoxarts().stream())
+                .reduce((boxArt, boxArt2) ->
+                        (boxArt.getHeight() * boxArt.getWidth()) > (boxArt2.getHeight() * boxArt2.getWidth()) ? boxArt : boxArt2)
+                .map(boxArt -> boxArt.getUrl()).orElse("");
+
+        System.out.println("Url: " + url);
+
+        return url;
     }
 }
