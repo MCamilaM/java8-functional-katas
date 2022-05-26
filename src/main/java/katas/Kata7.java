0427cbb426fec5fe8c68a02522b1f63283700461
Camila Morales <camila.moralesg21@gmail.com>
2022-05-26 00:13:04 -0500

    kata 7

--- src/main/java/katas/Kata7.java
+++ src/main/java/katas/Kata7.java
@@ -9,6 +9,7 @@ import util.DataUtil;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Retrieve the id, title, and smallest box art url for every video
@@ -19,6 +20,16 @@ public class Kata7 {
     public static List<Map> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
 
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys", "boxart", "url"));
+        List<Map> newListMovies = movieLists.stream().flatMap(movieList -> movieList.getVideos().stream())
+                .map(movie ->
+                    Map.of("id ", movie.getId(),
+                    "title ", movie.getTitle(),
+                    "boxart ", movie.getBoxarts().stream()
+                                    .reduce((boxArt, boxArt2) ->
+                                    (boxArt.getHeight() * boxArt.getWidth()) < (boxArt2.getHeight() * boxArt2.getWidth()) ? boxArt : boxArt2)
+                                    .map(boxArtUrl -> boxArtUrl.getUrl()))).collect(Collectors.toList());
+
+        System.out.println(newListMovies);
+        return newListMovies;
     }
 }
